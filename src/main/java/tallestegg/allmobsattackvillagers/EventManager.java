7377557409b3540ec4d5f96458a745acb3284558
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-04-05 09:51:59 -0700

    Add files via upload

--- src/main/java/tallestegg/allmobsattackvillagers/EventManager.java
+++ src/main/java/tallestegg/allmobsattackvillagers/EventManager.java
@@ -1,8 +1,11 @@
 package tallestegg.allmobsattackvillagers;
 
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.ai.goal.AvoidEntityGoal;
 import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
 import net.minecraft.entity.merchant.villager.AbstractVillagerEntity;
-import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.entity.monster.IMob;
+import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -12,12 +15,21 @@ public class EventManager
 	public static void onLivingSpawned(EntityJoinWorldEvent event) 
 	{
 		
-	if(event.getEntity() instanceof MonsterEntity) 
+	if (event.getEntity() instanceof IMob) 
 	{
-		MonsterEntity monster = (MonsterEntity)event.getEntity();
-	    monster.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(monster, AbstractVillagerEntity.class, false));
-	    
+		MobEntity mob = (MobEntity)event.getEntity();
+	    mob.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(mob, AbstractVillagerEntity.class, false));
+	    mob.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(mob, IronGolemEntity.class, false));
 	}
+	
+	   if(event.getEntity() instanceof AbstractVillagerEntity) 
+	   {
+		 AbstractVillagerEntity villager = (AbstractVillagerEntity)event.getEntity();
+		 villager.targetSelector.addGoal(3, new AvoidEntityGoal<>(villager, MobEntity.class, 6.0F, 1.0D, 1.2D, (mob) ->
+	  		{
+	  			return mob instanceof IMob;
+	  		}));
+	   }
 }
 
 }
